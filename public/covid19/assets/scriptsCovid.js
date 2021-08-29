458b00705d87d7e145ad5801c76ff6e394d7c895
Kasteish <by.zycko@gmail.com>
2021-08-29 11:29:16 -0400

    arreglos visuales y modal

--- public/covid19/assets/scriptsCovid.js
+++ public/covid19/assets/scriptsCovid.js
@@ -16,33 +16,42 @@ const mostrarModal = (pais) => {
         text:`Datos de ${data.location}`
       },
       axisY: {
-        title: "Personas",
+        //title: "Personas",
         titleFontColor: "#4F81BC",
         lineColor: "#4F81BC",
         labelFontColor: "#4F81BC",
         tickColor: "#4F81BC",
         includeZero: true
       },
+      axisY2: {
+        //title: "Casos muertos",
+        titleFontColor: "#C0504E",
+        lineColor: "#C0504E",
+        labelFontColor: "#C0504E",
+        tickColor: "#C0504E"
+    },
       toolTip: {
         shared: true
       },
       legend: {
         cursor: "pointer",
+        verticalAlign: "top",
         itemclick: toggleDataSeries
       },
       data: [
         {
           type: "column",
-          name: "Confirmados",
+          name: "Casos Confirmados",
           showInLegend: true,      
           yValueFormatString: "#,##0.# Casos",
           dataPoints: [{y: data.confirmed, label: 'Confirmados'}]//se le agrega el arreglo
         },
         {
           type: "column",
-          name: "Muertos",
+          name: "Casos Muertos",
           showInLegend: true,      
           yValueFormatString: "#,##0.# Casos",
+          axisYType: "secondary",
           dataPoints: [{y: data.deaths, label: 'Muertos'}]//X2
         },
       ] 
